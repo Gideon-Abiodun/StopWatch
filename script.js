55285ab2616590d6a4c68fa2eeacfd9c7e22c34f
Gideon-Abiodun <skuriangideon@gmail.com>
2023-07-11 17:10:06 +0100

    add event listener

--- script.js
+++ script.js
@@ -1,41 +1,70 @@
-let [seconds, minutes, hours] = [0,0,0];
-        let displayTime = document.getElementById("displayTime");
-        let timer = null
-        
-        function stopwatch(){
-            seconds++;
-            if(seconds == 60){
-                seconds = 0;
-                minutes++;
-                if(minutes == 60){
-                    minutes = 0;
-                    hours++;
-                }
-            }
+let [seconds, minutes, hours] = [0, 0, 0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
 
-            let h = hours < 10 ?  "0" + hours : hours;
-            let m = minutes < 10 ?  "0" + minutes : minutes;
-            let s = seconds < 10 ?  "0" + seconds : seconds;
+function stopwatch() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+
+// Variables for icons
+
+const stopBtn = document.getElementById("stopBtn");
+const startBtn = document.getElementById("startBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+// start function
+
+startBtn.addEventListener("click", () => {
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  timer = setInterval(stopwatch, 1000);
+});
+
+//    function watchstart(){
+//     if(timer!== null)
+//     {
+//         clearInterval(timer);
+//     }
+//     timer = setInterval(stopwatch,1000);
+// };
 
 
-            displayTime.innerHTML = h +":"+ m +":"+ s;
-        }
+// Stop Function
 
-        function watchstart(){
-            if(timer!== null)
-            {
-                clearInterval(timer);
-            }
-            timer = setInterval(stopwatch,1000);
-        }
+stopBtn.addEventListener("click", ()=>{
+    clearInterval(timer);
+})
 
-        function watchstop(){
-            clearInterval(timer);
-        }
-        function watchReset(){
-            clearInterval(timer);
-            [seconds, minutes, hours] = [0,0,0];
-            displayTime.innerHTML = "00:00:00";
-        }
-    
-    
+// function watchstop() {
+//   clearInterval(timer);
+// }
+
+
+// Reset Function
+
+resetBtn.addEventListener("click", ()=>{
+    clearInterval(timer);
+  [seconds, minutes, hours] = [0, 0, 0];
+  displayTime.innerHTML = "00:00:00";
+});
+
+// function watchReset() {
+//   clearInterval(timer);
+//   [seconds, minutes, hours] = [0, 0, 0];
+//   displayTime.innerHTML = "00:00:00";
+// }
